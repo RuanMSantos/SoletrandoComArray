911043544dc0dae30d60ae2456b41fdb11cbff15
RuanMSantos <ruancauancauan@gmail.com>
2024-09-12 17:54:22 -0300

    Finaliza programa

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Digite uma palavra: ");
+string palavraDigitada = Console.ReadLine()!.ToLower().Trim();
+
+for (int i = 0; i < palavraDigitada.Length; i++)
+{
+    char caracter = palavraDigitada[i];
+    Thread.Sleep(500);
+    Console.Write($"{caracter}");
+    Thread.Sleep(500);
+    if(i < palavraDigitada.Length-1)Console.Write("-");
+}
